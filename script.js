6ad38930ef63d04c1385f20325e1d1f6c4fb80f4
Seçkin Coskun <seckin.coskun.it@hotmail.com>
2023-10-01 12:10:35 +0200

    image-website.improvements
    everything except the showImage function and the styles of it, is done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let images = ['./img/img1.jpg', './img/img2.jpg', './img/img3.jpg', './img/img4.jpg', './img/img5.jpg', './img/img6.jpg', './img/img7.jpg', './img/img8.jpg',
               './img/img9.jpg', './img/img10.jpg', './img/img11.jpg', './img/img12.jpg', './img/img13.jpg', './img/img14.jpg', './img/img15.jpg', './img/img16.jpg',
-              './img/img17.jpg', './img/img18.jpg', './img/img19.jpg', './img/img20.jpg', './img/img21.jpg', './img/img22.jpg', './img/img23.jpg', './img/im24.jpg',
+              './img/img17.jpg', './img/img18.jpg', './img/img19.jpg', './img/img20.jpg', './img/img21.jpg', './img/img22.jpg', './img/img23.jpg', './img/img24.jpg',
               './img/img25.jpg', './img/img26.jpg', './img/img27.jpg', './img/img28.jpg', './img/img29.jpg', './img/img30.jpg', './img/img31.jpg', './img/img32.jpg',]
 
 
@@ -14,6 +14,6 @@ function load() {
     }
 }
 
-function showImage() {
+function showImage(i) {
 
 }
